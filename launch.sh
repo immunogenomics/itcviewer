e5eea61614e4d63c179f567d01e3be6baa4a7807
Kamil Slowikowski <kslowikowski@gmail.com>
2018-11-29 16:30:38 -0500

    ignore .pdf files for launch.sh

--- launch.sh
+++ launch.sh
@@ -4,6 +4,7 @@ rsync -rlzuvh \
     --exclude=launch.sh \
     --exclude=*.Rproj \
     --exclude=*.png \
+    --exclude=*.pdf\
     --exclude=.* \
     ~/work/github.com/immunogenomics/itcviewer/ ig:/srv/shiny-server/itc/
 
